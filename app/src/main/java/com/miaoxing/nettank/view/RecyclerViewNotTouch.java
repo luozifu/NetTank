333b18cf284854061b2f6841ffbab47e0522a915
P-V-4\Administrator <317771182@qq.com>
2018-10-28 01:17:31 +0800

    将内层recyclerview事件处理交给外层

---
+++ app/src/main/java/com/miaoxing/nettank/view/RecyclerViewNotTouch.java
@@ -0,0 +1,33 @@
+package com.miaoxing.nettank.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @author : Administrator
+ * @Date : 2018/10/28 0028
+ */
+public class RecyclerViewNotTouch extends RecyclerView {
+
+    public RecyclerViewNotTouch(@NonNull Context context) {
+        super(context);
+    }
+
+    public RecyclerViewNotTouch(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecyclerViewNotTouch(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return false;
+    }
+}
